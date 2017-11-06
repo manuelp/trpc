21189afff94e125c060a118836c692e2054c6e33
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2017-11-06 23:37:49 +0100

    Added WAI main (w/ Warp)

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import qualified Data.Map as M
 import RPC
+import Network.Wai
+import Network.HTTP.Types
+import Network.Wai.Handler.Warp (run)
   
 getParams :: M.Map String String -> Maybe (String, String)
 getParams m = do
@@ -25,10 +29,23 @@ dummyInput :: Parameters
 dummyInput =
   Parameters (UserID "demo") (M.fromList [("x", "Hello, "), ("y", "RPC!")])
 
+-- main :: IO ()
+-- main = do
+--   case res of
+--     Just action -> fmap show action >>= putStrLn
+--     Nothing -> putStrLn "Procedure not found!"
+--   where
+--     res = execute register (ProcedureID ["demo", "concat"]) dummyInput
+
+app :: Application
+app _ respond = do
+    putStrLn "I've done some IO here"
+    respond $ responseLBS
+        status200
+        [("Content-Type", "text/plain")]
+        "Hello, Web!"  
+
 main :: IO ()
 main = do
-  case res of
-    Just action -> fmap show action >>= putStrLn
-    Nothing -> putStrLn "Procedure not found!"
-  where
-    res = execute register (ProcedureID ["demo", "concat"]) dummyInput
+    putStrLn $ "http://localhost:8080/"
+    run 8080 app
