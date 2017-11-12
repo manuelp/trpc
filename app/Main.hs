04faae1d936a242ddbe03af1a46e3ac20b0c2bfa
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2017-11-12 16:19:15 +0100

    Working basic RPC

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,7 @@ import Network.Wai
 import Network.HTTP.Types
 import Network.Wai.Handler.Warp (run)
 import Data.ByteString.Char8 (unpack)
+import qualified Data.Text as T
 import Data.Aeson (encode)
 
 main :: IO ()
@@ -21,17 +22,25 @@ main = do
 --    = Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
 app :: Application
 app request respond = do
-  putStrLn "I've done some IO here"
+  putStrLn . show . queryString $ request
+  putStrLn . show $ path
   putStrLn . show $ params
-  res <- concatProcedure params
-  respond $
-    responseLBS
-      status200
-      [("Content-Type", "application/json")]
-      (encode . show $ res)
+  fn res >>= respond
   where
-    user = UserID "test"
+    path = ProcedureID . map (T.unpack) . pathInfo $ request
     params = getParameters (UserID "test") . queryString $ request
+    res :: Maybe (IO JsonString)
+    res = execute register path params
+
+buildResponse :: Status -> JsonString -> Response
+buildResponse s j =
+  responseLBS s [("Content-Type", "application/json")] (encode . show $ j)
+
+fn :: Maybe (IO JsonString) -> IO Response
+fn (Just x) = do
+  res <- x
+  return $ buildResponse status200 res
+fn Nothing = return . buildResponse status404 . JsonString $ "Unknown procedure"
 
 -- https://www.stackage.org/haddock/lts-9.12/http-types-0.9.1/Network-HTTP-Types-URI.html#t:Query
 --
@@ -48,6 +57,11 @@ getParameters u q =
 -- ----------------------------------------
 --
 
+register :: ProcedureRegister
+register =
+  ProcedureRegister $
+  M.fromList [(ProcedureID ["demo", "concat"], concatProcedure)]
+
 concatProcedure :: Procedure
 concatProcedure =
   \(Parameters _ m) ->
@@ -59,16 +73,3 @@ concatProcedure =
       x <- M.lookup "x" m
       y <- M.lookup "y" m
       return $ (x, y)
-
-register :: ProcedureRegister
-register =
-  ProcedureRegister $
-  M.fromList [(ProcedureID ["demo", "concat"], concatProcedure)]
-
--- main :: IO ()
--- main = do
---   case res of
---     Just action -> fmap show action >>= putStrLn
---     Nothing -> putStrLn "Procedure not found!"
---   where
---     res = execute register (ProcedureID ["demo", "concat"]) dummyInput
